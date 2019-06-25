9f4a566d2d9b5984167c3e98571fbaaef8c1b3f8
Hoang <I222317@NGIC.COM>
2019-06-25 16:02:35 -0500

    Initial Commit

---
+++ DatingApp.Api/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.Api
+{
+    //Entry point into the Application
+    public class Program
+    {
+        // When you run the application, this is the method that is invoked
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
